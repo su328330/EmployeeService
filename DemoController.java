083773051b4d9f586187e2bedf5305d6a993d337
su328330 <66730591+su328330@users.noreply.github.com>
2020-06-11 17:44:18 +0530

    Update DemoController.java

--- DemoController.java
+++ DemoController.java
@@ -27,19 +27,15 @@ public class DemoController {
 	@Autowired
 	EmpRepository empRepository;
 
-	// Getting all employees
-	
-	  @ApiOperation("Get All Empolyees")
-	  
-	  @GetMapping("/employees") public ResponseEntity<List<Employee>>
-	  getAllEmployes() { List<Employee> empList = null; empList =
-	  empService.getAllEmployes(); return ResponseEntity.ok(empList); }
-	 
-	
-	@GetMapping("/employees/{id}")
-	public ResponseEntity<Employee> retrieveEmployee (@RequestBody Employee emp) {
-			return ResponseEntity.ok(empRepository.save(emp));
-	}
+	/*
+	 * // Getting all employees
+	 * 
+	 * @ApiOperation("Get All Empolyees")
+	 * 
+	 * @GetMapping("/employees") public ResponseEntity<List<Employee>>
+	 * getAllEmployees() { List<Employee> empList = null; empList =
+	 * empService.getAllEmployes(); return ResponseEntity.ok(empList); }
+	 */
 	
 	@ApiOperation("Add new Empolyees")
 	@PostMapping("/employees")
@@ -47,7 +43,6 @@ public class DemoController {
 	return ResponseEntity.ok(empRepository.save(emp));
 	}
 		
-	
 	@ApiOperation("Update Empolyee")
 	@PutMapping("/employees/{id}")
 	public ResponseEntity<Employee>  updateEmployee(@RequestBody Employee emp) {
@@ -55,48 +50,40 @@ public class DemoController {
 	}
 
 	@ApiOperation("Delete Empolyee")
-	@DeleteMapping("/employees/{id}") 
-	ResponseEntity<String> deleteEmployee(@PathVariable("id") int id) { 
+	@DeleteMapping("/employees/{empid}") 
+	ResponseEntity<String> deleteEmployee(@PathVariable("empid") int id) { 
 	empRepository.deleteById(id);
 	return ResponseEntity.ok("Record deleted successfully"); }
 	 
 	
-/*
- * @ApiOperation("Get All Empolyees By Order")
- * 
- * @GetMapping("/employees") public ResponseEntity<List<Employee>>
- * getEmployeesByOrder() { List<Employee> empList =
- * empRepository.findAll(Sort.by(Sort.Direction.ASC, "name")); return
- * ResponseEntity.ok(empList); }
- */
-	
-	//List<Passenger> findByLastName(String lastName, Sort sort);
-	
-	/*
-	 * @ApiOperation("Get All Empolyees using PageRequest")
-	 * 
-	 * @GetMapping("/employees") public ResponseEntity<List<Employee>>
-	 * getAllEmployesPageReq() { List<Employee> empList = null; empList =
-	 * empService.getAllEmployesPageReq(1,10); return ResponseEntity.ok(empList); }
-	 */
-	
-	@ApiOperation("Get First Name of Employee")
-	@GetMapping("/employees")
-	public ResponseEntity<List<Employee>> employeeExpBelow5(){
-		return ResponseEntity.ok(empRepository.findByExp());		
+
+	@ApiOperation("Get All Empolyees By Order")
+	@GetMapping("/employees") public ResponseEntity<List<Employee>> getEmployeesByOrder() { 
+	List<Employee> empList = empRepository.findAll(Sort.by(Sort.Direction.ASC, "first_name")); 
+	return ResponseEntity.ok(empList); 
 	}
-	
-	@ApiOperation("Get First Name of Employee")
+ 
+	@ApiOperation("Get All Empolyees using PageRequest")
+	@GetMapping("/employees") public ResponseEntity<List<Employee>>
+	getAllEmployesPageReq() { List<Employee> empList = null; 
+	empList =empService.getAllEmployesPageReq(1,10); 
+	return ResponseEntity.ok(empList); }
+	 
+	@ApiOperation("Get Empolyees Below 5 years Exp")
+	@GetMapping("/employees") public ResponseEntity<List<Employee>>
+	employeeExpBelow5(){ return ResponseEntity.ok(empRepository.findByExp()); }
+  
+	@ApiOperation("Get Empolyees Above 10 years Exp")
 	@GetMapping("/employees")
-	public ResponseEntity<List<Employee>> getfindByExp10(){
-		return ResponseEntity.ok(empRepository.findByExp10());		
+	public ResponseEntity<List<Employee>>getfindByExp10(){ 
+	return ResponseEntity.ok(empRepository.findByExp10()); 
 	}
-	
-	@ApiOperation("Get First Name of Employee")
-	@GetMapping("/employees")
-	public ResponseEntity<List<Employee>> getfindByExpBetween(){
-		return ResponseEntity.ok(empRepository.findByExpBetween());		
+ 
+	@ApiOperation("Get Empolyees between 5 to 10 years Exp")
+	@GetMapping("/employees") 
+	public ResponseEntity<List<Employee>> getfindByExpBtwn(){
+	return ResponseEntity.ok(empRepository.findByExpBetween()); 
 	}
-	
+	 
 	
 }
